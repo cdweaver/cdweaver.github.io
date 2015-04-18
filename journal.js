1caf281505c3b3dc73ec1042df7dfc56e096d226
cdweaver <cdweaver@bu.edu>
2015-04-18 18:52:39 -0500

    more vars

--- journal.js
+++ journal.js
@@ -2,32 +2,20 @@ var userName = ko.observable();
 var userImage = ko.observable();
 var userEmail = ko.observable();
     
-function createUserObject(name, img, email) {
-    this.userName = name;
-    this.userImage = img;
-    this.userEmail = email;
-    console.log(userImage);
-}
-
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId());
   userName = (profile.getName());
   var userImage = ('<img src="' + profile.getImageUrl() + '">');
   var userEmail = ('Email: ' + profile.getEmail());
-  new createUserObject(name, userImage, userEmail);
-
-  
-
+ 
+console.log(userName);
 }
 
 function signOut() {
 				var auth2 = gapi.auth2.getAuthInstance();
 				auth2.signOut().then(function() {
 					console.log('User signed out.');
-					name = ("");
-  					userImage = ("");
-  					userEmail = ("");
 
 				});
 			}
