d6ed5719b588387fbbc0119ffee5c579280f6714
cdweaver <cdweaver@bu.edu>
2015-04-18 18:31:12 -0500

    Updates to user data

--- journal.js
+++ journal.js
@@ -1,13 +1,14 @@
 function createUserObject(userName, userImage, userEmail) {
     this.userName = ko.observable(userName);
     this.userImage = ko.observable(userImage);
+    this.userEmail = ko.observable(userEmail);
     console.log(userImage);
 }
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId());
-  var name = ('Name: ' + profile.getName());
+  var name = (profile.getName());
   var userImage = ('<img src="' + profile.getImageUrl() + '>');
   var userEmail = ('Email: ' + profile.getEmail());
   ko.applyBindings(new createUserObject(name, userImage, userEmail));
