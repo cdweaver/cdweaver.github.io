9b9d5100909ce69bc2c055bde725cad6d6c0ee86
cdweaver <cdweaver@bu.edu>
2016-09-24 17:42:24 -0500

    entries
    data

--- journal.js
+++ journal.js
@@ -1,34 +1,35 @@
-function createUserObject(userName, userImage, userEmail) {
-    this.userName = ko.observable(userName);
-    this.userImage = ko.observable(userImage);
-    this.userEmail = ko.observable(userEmail);
-    this.isSignedIn = ko.observable(true);
-    
-    this.entries = [{
-	title : "My Great Day",
-	date: "9/24/2016"
-},{
-	title: "My Horrible Day",
-	date: "9/23/2016"
-}];
+function createUserObject(userName, userImage, userEmail, entries) {
+	this.userName = ko.observable(userName);
+	this.userImage = ko.observable(userImage);
+	this.userEmail = ko.observable(userEmail);
+	this.isSignedIn = ko.observable(true);
+
+	this.entries = ko.observable(entries);
 }
 
 function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  var name = (profile.getName());
-  var userImage = ('<img src="' + profile.getImageUrl() + '">');
-  var userEmail = ('Email: ' + profile.getEmail());
-  ko.applyBindings(new createUserObject(name, userImage, userEmail));
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId());
+	var name = (profile.getName());
+	var userImage = ('<img src="' + profile.getImageUrl() + '">');
+	var userEmail = ('Email: ' + profile.getEmail());
 	
-  
+	var entries = [{
+		title : "My Great Day",
+		date : "9/24/2016"
+	}, {
+		title : "My Horrible Day",
+		date : "9/23/2016"
+	}];
+	
+	ko.applyBindings(new createUserObject(name, userImage, userEmail, entries));
 
 }
 
 function signOut() {
-				var auth2 = gapi.auth2.getAuthInstance();
-				auth2.signOut().then(function() {
-					console.log('User signed out.');
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+		console.log('User signed out.');
 
-				});
-			}
+	});
+}
